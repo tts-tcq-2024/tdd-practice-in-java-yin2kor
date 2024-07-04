2dd57352b3446c56df30885ecdb08b2418165470
Jain Rahul Vivek Kumar (MS/ECA1-XC) <RahulVivekKumar.Jain@in.bosch.com>
2024-07-04 16:39:20 +0530

    Created class but with unimplemented function

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -1,2 +1,8 @@
 package TddPracticeInJava;
 
+public class StringCalculator {
+
+    public int add(final String numbers) throws Exception {
+        throw new Exception("Not implemented");
+    }
+}
--- src/test/java/TddPracticeInJava/StringCalculatorTest.java
+++ src/test/java/TddPracticeInJava/StringCalculatorTest.java
@@ -8,8 +8,7 @@ public class StringCalculatorTest {
 
     
     @Test
-    public void ExpectZeroForEmptyInput()
-    {
+    public void ExpectZeroForEmptyInput() throws Exception {
         int expectedResult = 0;
         String input = "";
         StringCalculator objUnderTest = new StringCalculator();
@@ -17,56 +16,56 @@ public class StringCalculatorTest {
 
        assertEquals(expectedResult,result);
     }
-
-  @Test
-    public void ExpectZeroForSingleZero()
-    {
-        int expectedResult = 0;
-        String input = "0";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.add(input);
-       assertEquals(expectedResult,result);
-
-    }
-
-   @Test
-    public void ExpectSumForTwoNumbers()
-    {
-        int expectedResult = 3;
-        String input = "1,2";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.add(input);
-        assertEquals(expectedResult,result);
-    }
-
-   @Test
-    public void ExpectSumWithNewlineDelimiter()
-    {
-        int expectedResult = 6;
-        String input = "1\n2,3";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.add(input);
-         assertEquals(expectedResult,result);
-    }
-
- 
-    @Test
-    public void ignoreNumbersGreaterThan1000() {
-        int expectedResult = 1;
-        String input = "1,1001";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.add(input);
-
-       assertEquals(expectedResult,result);
-    }
-    @Test
-    public void ExpectSumWithCustomDelimiter()
-    {
-        int expectedResult = 3;
-        String input = "//;\n1;2";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.add(input);
-
-      assertEquals(expectedResult,result);
-    }
+//
+//  @Test
+//    public void ExpectZeroForSingleZero()
+//    {
+//        int expectedResult = 0;
+//        String input = "0";
+//        StringCalculator objUnderTest = new StringCalculator();
+//        int result = objUnderTest.add(input);
+//       assertEquals(expectedResult,result);
+//
+//    }
+//
+//   @Test
+//    public void ExpectSumForTwoNumbers()
+//    {
+//        int expectedResult = 3;
+//        String input = "1,2";
+//        StringCalculator objUnderTest = new StringCalculator();
+//        int result = objUnderTest.add(input);
+//        assertEquals(expectedResult,result);
+//    }
+//
+//   @Test
+//    public void ExpectSumWithNewlineDelimiter()
+//    {
+//        int expectedResult = 6;
+//        String input = "1\n2,3";
+//        StringCalculator objUnderTest = new StringCalculator();
+//        int result = objUnderTest.add(input);
+//         assertEquals(expectedResult,result);
+//    }
+//
+//
+//    @Test
+//    public void ignoreNumbersGreaterThan1000() {
+//        int expectedResult = 1;
+//        String input = "1,1001";
+//        StringCalculator objUnderTest = new StringCalculator();
+//        int result = objUnderTest.add(input);
+//
+//       assertEquals(expectedResult,result);
+//    }
+//    @Test
+//    public void ExpectSumWithCustomDelimiter()
+//    {
+//        int expectedResult = 3;
+//        String input = "//;\n1;2";
+//        StringCalculator objUnderTest = new StringCalculator();
+//        int result = objUnderTest.add(input);
+//
+//      assertEquals(expectedResult,result);
+//    }
 }

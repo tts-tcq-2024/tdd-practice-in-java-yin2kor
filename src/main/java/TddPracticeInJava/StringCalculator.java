067fa948c9075c9f67659fed9fa2fff2348f1125
Rahul Jain V <38587824+vrahul1998@users.noreply.github.com>
2024-07-08 22:37:23 +0530

    Default test cases passed

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -1,13 +1,72 @@
 package TddPracticeInJava;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
-    public static final int INVALID_STATUS = -1;
-    public int add(final String numbers) throws Exception {
-        int value = INVALID_STATUS;
-        if (numbers.isEmpty()) {
-            value = 0;
+    public static final String DELIMITER_REGEX = "\n|,";
+    public static final int MAX_NUMBER = 1000;
+
+    public static final String CUSTOM_DELIMITER_REGEX = "^//(.*)\\n";
+    // Compile the pattern
+    public static final Pattern PATTERN = Pattern.compile(CUSTOM_DELIMITER_REGEX);
+
+
+    public int add(final String numbers) {
+        int value = 0;
+        if (!numbers.isEmpty() && !"0".contentEquals(numbers)) {
+            String regex = appendToDelimiterRegex(getCustomDelimiter(numbers));
+            value = value + addSubsetNumbers(numbers.substring(getNumbersStartIndex(numbers)).split(regex));
         }
-       return value;
+        return value;
     }
+
+
+    private int addSubsetNumbers(String[] numbers) {
+        int value = 0;
+        for (String subNumber : numbers) {
+            int number = Integer.parseInt(subNumber);
+            if (number <= MAX_NUMBER) {
+                value = value + number;
+            }
+        }
+        return value;
+    }
+
+    String appendToDelimiterRegex(String customDelimiterRegex) {
+        String currentRegex = DELIMITER_REGEX;
+        if (!customDelimiterRegex.isEmpty()) {
+            currentRegex = currentRegex + "|" + customDelimiterRegex;
+        }
+        return currentRegex;
+    }
+
+    String getCustomDelimiter(String str) {
+        String regix = "";
+        Matcher matcher = getCustomDelimiterMatcher(str);
+        if (matcher.find()) {
+            regix = matcher.group(1);
+        }
+        return regix;
+    }
+
+    Matcher getCustomDelimiterMatcher(String s) {
+        return PATTERN.matcher(s);
+    }
+
+    int getNumbersStartIndex(String str) {
+        Matcher matcher = getCustomDelimiterMatcher(str);
+        int index = 0;
+        if (matcher.find()) {
+            index = matcher.end();
+        }
+        return index;
+    }
+
+    private String[] splitByDelimiter(String number, String regex) {
+        return number.split(regex);
+    }
+
+
 }
--- src/test/java/TddPracticeInJava/StringCalculatorTest.java
+++ src/test/java/TddPracticeInJava/StringCalculatorTest.java
@@ -15,56 +15,55 @@ public class StringCalculatorTest {
 
        assertEquals(expectedResult,result);
     }
-//
-//  @Test
-//    public void ExpectZeroForSingleZero()
-//    {
-//        int expectedResult = 0;
-//        String input = "0";
-//        StringCalculator objUnderTest = new StringCalculator();
-//        int result = objUnderTest.add(input);
-//       assertEquals(expectedResult,result);
-//
-//    }
-//
-//   @Test
-//    public void ExpectSumForTwoNumbers()
-//    {
-//        int expectedResult = 3;
-//        String input = "1,2";
-//        StringCalculator objUnderTest = new StringCalculator();
-//        int result = objUnderTest.add(input);
-//        assertEquals(expectedResult,result);
-//    }
-//
-//   @Test
-//    public void ExpectSumWithNewlineDelimiter()
-//    {
-//        int expectedResult = 6;
-//        String input = "1\n2,3";
-//        StringCalculator objUnderTest = new StringCalculator();
-//        int result = objUnderTest.add(input);
-//         assertEquals(expectedResult,result);
-//    }
-//
-//
-//    @Test
-//    public void ignoreNumbersGreaterThan1000() {
-//        int expectedResult = 1;
-//        String input = "1,1001";
-//        StringCalculator objUnderTest = new StringCalculator();
-//        int result = objUnderTest.add(input);
-//
-//       assertEquals(expectedResult,result);
-//    }
-//    @Test
-//    public void ExpectSumWithCustomDelimiter()
-//    {
-//        int expectedResult = 3;
-//        String input = "//;\n1;2";
-//        StringCalculator objUnderTest = new StringCalculator();
-//        int result = objUnderTest.add(input);
-//
-//      assertEquals(expectedResult,result);
-//    }
+
+  @Test
+    public void ExpectZeroForSingleZero() throws Exception {
+        int expectedResult = 0;
+        String input = "0";
+        StringCalculator objUnderTest = new StringCalculator();
+        int result = objUnderTest.add(input);
+       assertEquals(expectedResult,result);
+
+    }
+
+   @Test
+    public void ExpectSumForTwoNumbers()
+    {
+        int expectedResult = 3;
+        String input = "1,2";
+        StringCalculator objUnderTest = new StringCalculator();
+        int result = objUnderTest.add(input);
+        assertEquals(expectedResult,result);
+    }
+
+   @Test
+    public void ExpectSumWithNewlineDelimiter()
+    {
+        int expectedResult = 6;
+        String input = "1\n2,3";
+        StringCalculator objUnderTest = new StringCalculator();
+        int result = objUnderTest.add(input);
+         assertEquals(expectedResult,result);
+    }
+
+
+    @Test
+    public void ignoreNumbersGreaterThan1000() {
+        int expectedResult = 1;
+        String input = "1,1001";
+        StringCalculator objUnderTest = new StringCalculator();
+        int result = objUnderTest.add(input);
+
+       assertEquals(expectedResult,result);
+    }
+    @Test
+    public void ExpectSumWithCustomDelimiter()
+    {
+        int expectedResult = 3;
+        String input = "//;\n1;2";
+        StringCalculator objUnderTest = new StringCalculator();
+        int result = objUnderTest.add(input);
+
+      assertEquals(expectedResult,result);
+    }
 }
